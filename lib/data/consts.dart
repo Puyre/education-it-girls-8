7405b8d81e6127eb922eb7c4334cbd89251463ef
andrejdonskov <andnskv@gmail.com>
2023-03-25 15:06:48 +0600

    Добавил получение гифок с бека

---
+++ lib/data/consts.dart
@@ -0,0 +1,3 @@
+const baseUrl = "api.giphy.com/v1/gifs/trending";
+const apiKey = "8QxOcDeCWc6MA9wRgG6AEoHiv3aIHmq8";
+const limit = 12;
--- lib/main.dart
+++ lib/main.dart
@@ -1,33 +1,10 @@
+import 'package:education/screens/gif_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     const MaterialApp(
-      home: FirstScreen(),
+      home: GifScreen(),
     ),
   );
-}
-
-class FirstScreen extends StatefulWidget {
-  const FirstScreen({super.key});
-
-  @override
-  State<FirstScreen> createState() => _FirstScreenState();
-}
-
-class _FirstScreenState extends State<FirstScreen> {
-  @override
-  void initState() {
-    super.initState();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Списки"),
-      ),
-      body: const SizedBox.shrink(),
-    );
-  }
 }
